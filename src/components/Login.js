36056ff23efd7101cea15add37c69499516117e9
deepikarawal1435@gmail.com <gouravgarg093@gmail.com>
2023-07-30 16:03:40 +0530

    updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ const Login = (props) => {
           <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
         </CTA>
         <BgImage />
-      <span>~Gourav~ </span>
+      <span>~Deepika~ </span>
       </Content>
 
     </Container>
